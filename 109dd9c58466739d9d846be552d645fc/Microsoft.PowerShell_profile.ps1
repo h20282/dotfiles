9dddc2508f90270d1c4927e7405a796bff5a9683
Zhihou Guo <2053620282@qq.com>
2021-11-10 11:38:43 +0800

    power shell alias

---
+++ 109dd9c58466739d9d846be552d645fc/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,11 @@
+﻿
+Import-Module 'D:\posh-git\src\posh-git.psd1'
+
+
+function gst { git status }
+function gdf { git diff }
+
+function cmkb { cmake --build ./ }
+
+function cdd { cd d: }
+function cdc { cd c: }
