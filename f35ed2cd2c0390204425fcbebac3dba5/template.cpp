970fe72cc85a25a4c991d8cb7b7d8c0a4f391894
郭智侯 <guozhihou@cvte.com>
2024-01-26 09:54:02 +0800

    modify template.cpp

--- f35ed2cd2c0390204425fcbebac3dba5/template.cpp
+++ f35ed2cd2c0390204425fcbebac3dba5/template.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 
-using std::cin;
-using std::cout;
-using std::endl;
+inline void print(const char *f) {
+    std::cout << f;
+}
+
+template <typename First, typename... Rest>
+inline void print(const char *format, const First &first, const Rest &...rest) {
+    if (*format == '\0') { return; }
+    while (*format && !(format[0] == '{' && (format[1] == '}'))) {
+        putchar(*format++);
+    }
+    std::cout << first;
+    print(format + 2, rest...);
+}
 
 int main() {
-
+    print("{}, {}\n", "hello", "world");
     return 0;
 }
